b221773dd31b110cac375f80933543a9d9dbb8a0
MistEO <mistereo@hotmail.com>
2024-10-16 17:50:57 +0800

    fix(osx): -Wno-error=shorten-64-to-32

--- vcpkg-overlay/triplets/maa-arm64-osx.cmake
+++ vcpkg-overlay/triplets/maa-arm64-osx.cmake
@@ -10,3 +10,9 @@ string(APPEND VCPKG_C_FLAGS "-g -DNDEBUG")
 string(APPEND VCPKG_CXX_FLAGS "-g -DNDEBUG")
 
 include(${CMAKE_CURRENT_LIST_DIR}/maa-linux-library-override.cmake)
+
+if(PORT MATCHES "onnxruntime")
+    message("add -Wno-error=shorten-64-to-32 for ${PORT}")
+    string(APPEND VCPKG_C_FLAGS "-Wno-error=shorten-64-to-32")
+    string(APPEND VCPKG_CXX_FLAGS "-Wno-error=shorten-64-to-32")
+endif()
