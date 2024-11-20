62cc653f67d0c1fcffd6c5114cbf14f6a09ec3f6
MistEO <mistereo@hotmail.com>
2024-11-20 18:00:44 +0800

    chore: update fd

--- vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
+++ vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO MaaXYZ/FastDeploy
-    REF ecd60dfeac3474f8bb00d4eb89ad2bf1a69802f2
-    SHA512 f077ed0ed5faca43e3e943e7cc401c7dabb22e3b41a6728010e81de794919ef74db0f0643a36c2eec92aadf2b30ed276c3c63061757a19f08bcc761a62a74608
+    REF c2b1a52c4d742d98238485fafd2a520f34eada88
+    SHA512 326b937e1abbdd70dc6e4403fbd2578eafb4fd599c7be4571a6b3baf2ad199c745ff1a9e2146d0104cdf0fa7c9856f008aa395ad32b213b6184dcde4956a9ce6
 )
 
 vcpkg_cmake_configure(
