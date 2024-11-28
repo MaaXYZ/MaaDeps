d19787f42c048eab9472199bd7490a2e7c45b15f
MistEO <mistereo@hotmail.com>
2024-11-28 10:17:18 +0800

    rebase fastdeploy

--- vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
+++ vcpkg-overlay/ports/maa-fastdeploy/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO MaaXYZ/FastDeploy
-    REF 0d044abaefda8390b636cd177f5b3f4cce2125f4
-    SHA512 c077d7157976dc01d3de798e81cd4cab97dcadcb73ccf1e95b9fd028e089e1bfca27506afb3d92caa3e95d231d44b6f107d19d5016bfd7b527e86d82e70f7f9b
+    REF d593eb92245c626e0b31549d55f12fba086e63a2
+    SHA512 f40d66d4642f931cb2f1ed47b5d2e6b6d71f2d9d3c740dff1bdbf1fc9c43b994a40c7b47dc8c4cd870478743e975f7bd9ca0b33dfc35416df79b3fb844f828c7
 )
 
 vcpkg_cmake_configure(
