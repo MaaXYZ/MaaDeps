a34fcef33aa0ee8ffce5d9a2f8b71739f6c7371e
nekosu <liao.hy@outlook.com>
2025-09-12 20:34:50 +0800

    feat: custom built clang support

--- build.py
+++ build.py
@@ -78,6 +78,12 @@ def sdk_tarball():
         "./maadeps.cmake",
     ]
 
+    if 'linux' in session.target:
+        extra_files += [
+            *glob.glob("./cmake/*"),
+            "linux-toolchain-download.py"
+        ]
+
     if 'windows' in session.target:
         def sdk_filter(info: tarfile.TarInfo):
             if info.name.endswith(".pdb"):
