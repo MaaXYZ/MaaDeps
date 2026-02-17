f1f20fb8aed87e77c6b74aeefcfa9392cf6f9dc7
nekosu <liao.hy@outlook.com>
2026-02-18 00:15:34 +0800

    feat: update vcpkg to 2025.12.12 (#17)
    ## Summary by Sourcery
    
    Build:
    - 将 vcpkg 引用更新到较新的版本，使依赖管理工具与最新发布版本保持一致。
    
    <details>
    <summary>Original summary in English</summary>
    
    ## Summary by Sourcery
    
    Build:
    - Bump vcpkg reference to a newer version to align dependency tooling
    with the latest release.
    
    </details>

--- vcpkg-overlay/triplets/maa-arm64-linux.cmake
+++ vcpkg-overlay/triplets/maa-arm64-linux.cmake
@@ -2,7 +2,10 @@ set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_BUILD_TYPE release)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-set(VCPKG_MAKE_BUILD_TRIPLET "--host=x86_64-linux-gnu --target=aarch64-linux-gnu")
+set(VCPKG_MAKE_BUILD_TRIPLET "--target=aarch64-linux-gnu")
+if (PORT STREQUAL "pthread-stubs")
+  set(VCPKG_MAKE_BUILD_TRIPLET "--host=x86_64-linux-gnu --target=x86_64-linux-gnu")
+endif ()
 
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../../cmake/maa-arm64-linux-toolchain.cmake")
 
